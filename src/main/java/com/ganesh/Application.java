796ba087fd5b1f77f79b79f534bb1c56e8207e89
TheGanesh <ganesh.kandisa@gmail.com>
2015-12-14 21:20:44 -0600

    My first spring boot commit

---
+++ src/main/java/com/ganesh/Application.java
@@ -0,0 +1,15 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ganesh on 12/14/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
